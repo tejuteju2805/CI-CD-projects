3c87a24c0e76a4b38fc223ad02616dde8027f75c
tejuteju2805 <tejaswikaligineedi32gmail.com>
2026-01-08 15:13:45 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -5,12 +5,28 @@ namespace TestCICD
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("Setup executed");
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            int result = Add(5, 10);
+            Assert.AreEqual(15, result);
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        [Test]
+        public void ShowSumTest()
+        {
+            int a = 5;
+            int b = 10;
+            int sum = a + b;
+
+            Console.WriteLine("Sum = " + sum);
+            Assert.AreEqual(15, sum);
         }
     }
 }
