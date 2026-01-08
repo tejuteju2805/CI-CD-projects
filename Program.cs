24bb851b12ae63e3aa465d0cbc84662a5a412161
tejuteju2805 <tejaswikaligineedi32gmail.com>
2026-01-08 12:29:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace TestCICD
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
